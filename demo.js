003e769b4e70a5e8acb2cda8feb7fbda7380d4f8
Mattia Pazienti <kuttebo@msn.com>
2013-12-17 16:12:54 +0100

    Update demo.js

--- demo.js
+++ demo.js
@@ -1,20 +1,21 @@
 (function ($) {
 
+   // check device
    var check = {any:function(){
        return navigator.userAgent.match(/Android|BlackBerry|iPhone|iPad|iPod|Opera Mini|IEMobile/i);
    }};
    if(check.any()) {
         lnt = $(".tch");
         lnt.bind("click.a", function(event) {
-          event.preventDefault();
-          $(this).unbind(".a");
+          event.preventDefault(); // disable first touch
+          $(this).unbind(".a");  // allow second touch
         });
 
         lnt.click(function(e) {
            if ($(this).hasClass('focus')) {
                $(this).removeClass('focus');
            } else {
-               $(this).addClass('focus');
+               $(this).addClass('focus'); // this class works as hover effect
            }
         });
    };
